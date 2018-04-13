1870b3cf618c313fa1908da4e149cae1a14e8379
Gloria Alcivar <sukkie_hye501@hotmail.com>
2018-04-13 17:23:36 -0500

    v3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,37 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NewsPage } from '../news/news';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
+
 export class HomePage {
 
+  
   constructor(public navCtrl: NavController) {
     
   }
-  
-  
-  
-  presentLoadingDefault() {
-  let loading = this.loadingCtrl.create({
-    content: 'Please wait...'
-  });
 
-  loading.present();
-
-  setTimeout(() => {
-    loading.dismiss();
-  }, 5000);
-}
-
-
-
-doRefresh(refresher){
+    doRefresh(refresher){
        setTimeout(() => {
          console.log('Async operation has ended');
          refresher.complete();
        }, 2000);
+    }
+    
+    showNewsPage(){
+        this.navCtrl.push(NewsPage);
     }
 
 }
