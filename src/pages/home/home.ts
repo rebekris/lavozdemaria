ac11db2fea72565409affc002dfe4d272605a1f6
Gloria Alcívar <sukkie_hye501@hotmail.com>
2018-04-24 14:55:13 -0500

    24/04 v3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,12 +26,19 @@ export class HomePage {
     }
     
     showNewsPage(article){
-        this.navCtrl.push(NewsPage);
+        this.navCtrl.push(
+            NewsPage,
+            {
+                art : article
+            }
+            );
+        
     }
     
     
       ionViewDidEnter(){
-        this.provider.getArticles('mihttp.net/demo/lavozdemaria/end/get/content/tagarticles?tagid=2').subscribe(result=>{
+        this.provider.getArticles('mihttp.net/demo/lavozdemaria/end/get/content/tagarticles?tagid=2').subscribe(
+        result=>{
             this.news= result.articles;
             console.log("Texto: " + this.news + "...");
             },
